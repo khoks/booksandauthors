68ffe42d40d858fba4d8d9806b785d1c5134ad44
khoks <rahulsinghkhokhar@gmail.com>
2020-06-13 14:40:23 -0400

    second commit to test via eclipse

--- src/main/java/khoks/com/test/graphqlbackend/booksandauthors/BooksandauthorsApplication.java
+++ src/main/java/khoks/com/test/graphqlbackend/booksandauthors/BooksandauthorsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BooksandauthorsApplication {
 
+	//added a sample comment
 	public static void main(String[] args) {
 		SpringApplication.run(BooksandauthorsApplication.class, args);
 	}
