3d7de4faf90fd254bc39d0800a7a08d915728217
khoks <rahulsinghkhokhar@gmail.com>
2020-06-13 14:27:23 -0400

    first commit

---
+++ src/test/java/khoks/com/test/graphqlbackend/booksandauthors/BooksandauthorsApplicationTests.java
@@ -0,0 +1,13 @@
+package khoks.com.test.graphqlbackend.booksandauthors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksandauthorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
